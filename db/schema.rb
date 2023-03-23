8cc8c10d5d220ae78eee8c2de10bad1aa3b9a798
shingo inoue <zhangmoutou@yahoo.co.jp>
2023-03-24 08:53:11 +0900

    質問詳細画面にお気に入り追加完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_23_044620) do
+ActiveRecord::Schema.define(version: 2023_03_23_233437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,9 @@ ActiveRecord::Schema.define(version: 2023_03_23_044620) do
     t.bigint "answer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "question_id", null: false
     t.index ["answer_id"], name: "index_favorites_on_answer_id"
+    t.index ["question_id"], name: "index_favorites_on_question_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
@@ -90,6 +92,7 @@ ActiveRecord::Schema.define(version: 2023_03_23_044620) do
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
   add_foreign_key "favorites", "answers"
+  add_foreign_key "favorites", "questions"
   add_foreign_key "favorites", "users"
   add_foreign_key "questions", "categories"
   add_foreign_key "questions", "users"
