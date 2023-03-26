07f0ca36b0841bfc6aaadd91042999b62621d970
shingo inoue <zhangmoutou@yahoo.co.jp>
2023-03-26 10:12:40 +0900

    管理者用ログイン画面ルーティング追加完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  devise_for :admins   
   root 'home#index'   
 
   resources :questions do
